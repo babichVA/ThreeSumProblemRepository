5cb1f7d6921fa8bad849c5d580a2e7303b2cd23a
Володимир Бабич <volodymyrbabych64@gmail.com>
2020-12-04 22:38:08 +0200

    -add template

---
+++ ThreeSumProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThreeSumProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
